908bf82428fd5ae28dc7a50db6b057bcd46e0e6a
Eric Qian <eric@enumc.com>
2018-02-22 21:16:01 -0800

    Initial File

---
+++ BabyBeats.ino
@@ -0,0 +1,67 @@
+/*************************************************************
+  Download latest Blynk library here:
+    https://github.com/blynkkk/blynk-library/releases/latest
+
+  Blynk is a platform with iOS and Android apps to control
+  Arduino, Raspberry Pi and the likes over the Internet.
+  You can easily build graphic interfaces for all your
+  projects by simply dragging and dropping widgets.
+
+    Downloads, docs, tutorials: http://www.blynk.cc
+    Sketch generator:           http://examples.blynk.cc
+    Blynk community:            http://community.blynk.cc
+    Follow us:                  http://www.fb.com/blynkapp
+                                http://twitter.com/blynk_app
+
+  Blynk library is licensed under MIT license
+  This example code is in public domain.
+
+ *************************************************************
+  This example runs directly on ESP8266 chip.
+
+  Note: This requires ESP8266 support package:
+    https://github.com/esp8266/Arduino
+
+  Please be sure to select the right ESP8266 module
+  in the Tools -> Board menu!
+
+  NOTE: SmartConfig might not work in your environment.
+        Please try basic ESP8266 SmartConfig examples
+        before using this sketch!
+
+  Change Blynk auth token to run :)
+
+ *************************************************************/
+
+/* Comment this out to disable prints and save space */
+#define BLYNK_PRINT Serial
+
+
+#include <ESP8266WiFi.h>
+#include <DNSServer.h>            //Local DNS Server used for redirecting all requests to the configuration portal
+#include <ESP8266WebServer.h>     //Local WebServer used to serve the configuration portal
+#include <WiFiManager.h>          //https://github.com/tzapu/WiFiManager WiFi Configuration Magic
+#include <BlynkSimpleEsp8266.h>
+
+// You should get Auth Token in the Blynk App.
+// Go to the Project Settings (nut icon).
+char auth[] = "2211a13ba6ed4254a8b3b9a0127dc56c";
+WiFiManager wifiManager;
+
+void setup()
+{
+  // Debug console
+  Serial.begin(9600);
+
+  wifiManager.autoConnect("prototype_babyBeats");
+
+  WiFi.printDiag(Serial);
+
+  Blynk.config(auth);
+}
+
+void loop()
+{
+  Blynk.run();
+}
+
